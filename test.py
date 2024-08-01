cce564a05a026588a19977a4a46a119be83d00ca
Varun Ramesh <varunramesh@live.com>
2024-07-31 20:07:17 -0700

    Number formatting test.

--- test.py
+++ test.py
@@ -16,6 +16,8 @@ class TestMagicLamp(unittest.TestCase):
         self.assertEqual(format_name("MCDOWELL"), "McDowell")
 
     def test_stability(self):
+        """Check if a model actually returns a consistent answer."""
+
         format_name = magic_lamp.Function(
             "Format this surname in a way that it would be written out.",
             examples=[
@@ -27,6 +29,27 @@ class TestMagicLamp(unittest.TestCase):
 
         for i in range(100):
             self.assertEqual(format_name("MCDOWELL"), "McDowell")
+
+    def test_models_cache(self):
+        """Ensure that loaded models are being cached and not re-loaded for every function."""
+
+        functions = []
+        for i in range(20):
+            functions.append(
+                magic_lamp.Function(
+                    "Write this number out in words.",
+                    examples=[
+                        ("1", "one"),
+                        ("35", "thirty-five"),
+                        ("15,690", "fifteen thousand, six hundred ninety"),
+                    ],
+                )
+            )
+
+        self.assertEqual(
+            functions[0]("328,745,226,793"),
+            "three hundred twenty-eight billion, seven hundred forty-five million, two hundred twenty-six thousand, seven hundred ninety-three",
+        )
 
 
 if __name__ == "__main__":
